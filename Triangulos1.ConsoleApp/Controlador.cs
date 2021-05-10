7db2b8931e55914b88fd75704db0ee33db5585e8
RuanMedTI <rurato2002@gmail.com>
2021-05-10 15:29:16 -0300

    Add project files.

---
+++ Triangulos1.ConsoleApp/Controlador.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulos1.ConsoleApp
+{
+    public class Controlador
+    {
+        //insert dos valores
+        public int x;
+        public int y;
+        public int z;
+
+        //validacao das variaveis com os valores
+        public void Calculo()
+        {
+            if (x == y && y == z)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Triangulo Equilátero");
+                Console.ResetColor();
+            }
+
+            if (x != y || y != z || x != z)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Triangulo Isóceles");
+                Console.ResetColor();
+            }
+
+            else if (x != y || y != z || x != z)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Triangulo Escaleno");
+                Console.ResetColor();
+            }
+
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Triangulo Inválido");
+                Console.ResetColor();
+            }
+        }
+    }
+}
+
---
+++ Triangulos1.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulos1.ConsoleApp
+{
+    class Program
+    {
+        static Controlador controladores = new Controlador();
+        static void Main(string[] args)
+        {
+            controladores.x = 5;
+            controladores.y = 5;
+            controladores.z = 2;
+
+            controladores.Calculo();
+        }
+    }
+}
+
