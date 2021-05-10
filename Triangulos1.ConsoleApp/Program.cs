0cf624ba43fe946b314ba54593a5ae2842ed39f4
RuanMedTI <rurato2002@gmail.com>
2021-05-10 16:39:22 -0300

    returns

--- Triangulos1.ConsoleApp/Program.cs
+++ Triangulos1.ConsoleApp/Program.cs
@@ -8,18 +8,19 @@ namespace Triangulos1.ConsoleApp
 {
     class Program
     {
-        static Controlador controladores = new Controlador();
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("Triangulo 1.1\n");
             Console.ResetColor();
 
-            controladores.lado1 = 5;
-            controladores.lado2 = 5;
-            controladores.lado3 = 2;
+            Triangulo triangulos = new Triangulo();
 
-            controladores.Calculo();
+            triangulos.lado1 = 5;
+            triangulos.lado2 = 5;
+            triangulos.lado3 = 2;
+
+            triangulos.Calculo();
 
             Console.ReadLine();
         }
---
+++ Triangulos1.ConsoleApp/Triangulo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulos1.ConsoleApp
+{
+    public class Triangulo
+    {
+        //insert dos valores
+        public int lado1;
+        public int lado2;
+        public int lado3;
+
+        //validacao das variaveis com os valores
+        public string Calculo()
+        {
+
+            if (lado1 == lado2 && lado2 == lado3)
+            {               
+                return "Triangulo Equilátero";                
+            }
+
+            if (lado1 != lado2 || lado2 != lado3 || lado1 != lado3)
+            {                
+                return "Triangulo Isóceles";               
+            }
+
+            else if (lado1 != lado2 || lado2 != lado3 || lado1 != lado3)
+            {               
+                return "Triangulo Escaleno";                
+            }
+
+            else
+            {               
+                return "Triangulo Inválido";                
+            }
+        }
+    }
+}
+
