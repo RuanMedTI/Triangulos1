423eeaa14a2b3834f7f4129809737f4b221e5d9d
RuanMedTI <rurato2002@gmail.com>
2021-05-10 15:37:49 -0300

    commit

--- Triangulos1.ConsoleApp/Program.cs
+++ Triangulos1.ConsoleApp/Program.cs
@@ -11,11 +11,17 @@ namespace Triangulos1.ConsoleApp
         static Controlador controladores = new Controlador();
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Triangulo 1.1\n");
+            Console.ResetColor();
+
             controladores.x = 5;
             controladores.y = 5;
             controladores.z = 2;
 
             controladores.Calculo();
+
+            Console.ReadLine();
         }
     }
 }
